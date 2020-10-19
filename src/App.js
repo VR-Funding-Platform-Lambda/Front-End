4fa514faf0c6c3522ea5720aaca51dd0dbf42d2a
Ramsha <ramshanasir.rn@gmail.com>
2020-10-19 14:12:44 -0500

    signUp page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import React from 'react';
-import Login from './components/LoginForm'
+import React, { Component } from 'react';
+import { HashRouter as Router, Route, Link, NavLink } from 'react-router-dom';
+import LoginForm from './Components/LoginForm'
+import SignUpForm from './Components/SignupForm';
 
-function App() {
-  return (
-    <div className="App">
-      <Login />
-    </div>
-  );
-}
 
-export default App;
+
+  class App extends Component {
+    render() {
+      return (
+        <Router basename="/react-auth-ui/">
+          <div className="App">
+            <div className="App__Aside"></div>
+            <div className="App__Form">
+              <div className="PageSwitcher">
+                <NavLink to="/sign-in" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">Sign In</NavLink>
+                <NavLink exact to="/" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">Sign Up</NavLink>
+              </div>  
+              <Route exact path="/" component={SignUpForm}>
+              </Route>
+              <Route path="/sign-in" component={LoginForm}>
+              </Route>
+            </div>
+  
+          </div>
+        </Router>
+      );
+    }
+  }
+
+  
+  export default App;

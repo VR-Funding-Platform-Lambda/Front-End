30b5872b7ce3c9babee23698edde1f24d8260d87
Ramsha <ramshanasir.rn@gmail.com>
2020-10-22 11:58:00 -0500

    cypress installed

--- src/validation/FormSchema.js
+++ src/validation/FormSchema.js
@@ -1,10 +1,8 @@
 import * as yup from 'yup';
 
-const loginSchema = yup.object().shape({
-    name:yup.string().name('Please enter your name').required('Please enter your name'),
+const formSchema = yup.object().shape({
     email: yup.string().email('Please enter a valid email address').required('Please enter an email address'),
-    password: yup.string().min(6,'Please enter a valid password').required('Please enter a password')
+    password: yup.string().min(6,'Please enter a valid password').required('Please enter a password'),
+    fullName: yup.string().min(3,'Please enter name that is longer than 3 characters').required('Please enter name that is longer than 3 characters')
 });
-
-
-export default loginSchema;
+export default formSchema;

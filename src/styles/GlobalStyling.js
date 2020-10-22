b8740b0aa977a1929053c89fc3ad1d5f33ef48eb
gerardo-rivera3901 <gerardo.rivera3901@gmail.com>
2020-10-22 10:57:20 -0700

    Added a bit of styling

--- src/styles/GlobalStyling.js
+++ src/styles/GlobalStyling.js
@@ -1,11 +1,21 @@
 import {createGlobalStyle} from 'styled-components'
 
 const GlobalStyle = createGlobalStyle`
-  color: blueviolet;
+
   body {
     margin: 0;
     padding: 0;
     background: #343434;
+    font-family: 'Montserrat', sans-serif;
+    font-size: 1.4rem;
+  }
+  input {
+    font-family: 'Montserrat', sans-serif;
+    font-size: 1.2rem;
+  }
+  button {
+    font-family: 'Montserrat', sans-serif;
+    font-size: 1.2rem;
   }
 `;
 
